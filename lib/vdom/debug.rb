29ab76e16164b0a9e44f3f0191132cd6fd05667b
Andreas Alin <andreas.alin@gmail.com>
2023-03-07 00:45:48 -0500

    attempt to fix text diff

---
+++ lib/vdom/debug.rb
@@ -0,0 +1,41 @@
+require "digest/sha2"
+require "singleton"
+
+module VDOM
+  class Debug
+    include Singleton
+
+    def self.enabled? =
+      Fiber[:vdom_debug_enabled?] != false
+
+    def self.disable(&)
+      prev = Fiber[:vdom_debug_enabled?]
+      Fiber[:vdom_debug_enabled?] = false
+      yield
+    ensure
+      Fiber[:vdom_debug_enabled?] = prev
+    end
+
+    def d(**kwargs)
+      return unless self.class.enabled?
+
+      kwargs.map do |key, value|
+        "#{colorize(key)}: #{value.inspect}"
+      end.join(", ").then { puts "{#{_1}}" }
+    end
+
+    def colorize(str)
+      "\e[38;5;#{color(str)}m#{str}\e[0m"
+    end
+
+    def color(str)
+      16 + Digest::SHA256.digest(str.to_s).bytes.first % 200
+    end
+
+    module Refinements
+      refine Kernel do
+        def d(**) = Debug.instance.d(**)
+      end
+    end
+  end
+end
--- lib/vdom/text_diff.rb
+++ lib/vdom/text_diff.rb
@@ -1,44 +1,108 @@
-#!/usr/bin/env ruby -rbundler/setup
+#!/usr/bin/env ruby -rbundler/setup -rpry
 # frozen_string_literal: true
 
 require "diff/lcs"
 require_relative "patches"
+require_relative "debug"
+
+using VDOM::Debug::Refinements
 
 module VDOM
   class TextDiff
     def self.diff(node_id, seq1, seq2, &)
       # NOTE: This implementation is buggy.
       # Indexes and counts are incorrect sometimes.
-      Diff::LCS.diff(seq1, seq2).each do |changes|
-        deleting = changes.select(&:deleting?)
-        inserting = changes.reject(&:deleting?)
-        replacement = inserting.map(&:element).join
+      offset = 0
+
+      ai = bj = 0
+      res = String.new
+
+      Diff::LCS.diff(seq1, seq2).each do |changeset|
+        insertions = String.new
+
+        ais = ai
+        bjs = bj
+
+        changeset.each do |change|
+          case
+          when change.deleting?
+            while ai < change.position
+              res << seq1[ai, 1]
+              ai += 1
+              bj += 1
+            end
+
+            ai += 1
+
+          when change.adding?
+            while bj < change.position
+              res << seq1[ai, 1]
+              ai += 1
+              bj += 1
+            end
+
+            bj += 1
+
+            res << change.element
+          end
+
+          d(res:)
+        end
+
+        deleting = changeset.select(&:deleting?)
+        adding = changeset.select(&:adding?)
+
+        d(ai:, ais:, deleting:)
+        d(bj:, bjs:, adding:)
+        ax = ai - ais
+        bx = bj - bjs
+        d(ax:, bx:)
+        d(d: bx - ax)
+        d(offset:)
+        replacement = adding.map(&:element).join
         deletion = deleting.map(&:element).join
 
         if deleting.empty?
+          start = adding.map(&:position).min + offset
+          offset += replacement.length
+
           next yield Patches::InsertData[
             node_id,
-            inserting.map(&:position).min,
+            start,
             replacement
           ]
         end
 
-        if inserting.empty?
+        if adding.empty?
+          start = deleting.map(&:position).min + offset
+
           next yield Patches::DeleteData[
             node_id,
-            deleting.map(&:position).min,
-            deleting.map(&:position).max - deleting.map(&:position).min,
+            start,
+            deletion.length
           ]
         end
 
+        start = adding.map(&:position).min + offset
+
         yield Patches::ReplaceData[
           node_id,
-          inserting.map(&:position).min,
+          start,
           deletion.length,
           replacement
         ]
+      ensure
+        d()
+        offset += bx - ax
       end
 
+      while ai < seq1.length
+        res << seq1[ai, 1]
+        ai += 1
+        bj += 1
+      end
+
+      d(res:)
       seq2
     end
   end
@@ -49,17 +113,35 @@ if __FILE__ == $0
 
   class VDOM::TextDiff::Test < Minitest::Test
     def test_diff
-      assert_diffed("foobar", "")
-      assert_diffed("", "foobar")
-      assert_diffed("foo", "foobar")
-      assert_diffed("foobar", "foo")
-      assert_diffed("foobaz", "foobarbaz")
-      assert_diffed("foobaz", "foobarbaz")
-      assert_diffed("tjosannnnn", "tjohejsannnn")
+      VDOM::Debug.disable do
+        assert_diffed("foobar", "")
+        assert_diffed("", "foobar")
+        assert_diffed("foo", "foobar")
+        assert_diffed("foobar", "foo")
+        assert_diffed("foobaz", "foobarbaz")
+        assert_diffed("foobaz", "foobarbaz")
+        assert_diffed("tjosannnnn", "tjohejannnn")
+      end
+    end
+
+    def test_diff_random
+      words =
+        File.join(__dir__, "..", "..", "app", "words.txt")
+          .then { File.read(_1) }
+          .split.shuffle
+          .first(20)
+          .map(&:strip)
+
+      words.reduce("") do |seq1, seq2|
+        p [seq1, seq2]
+        assert_diffed(seq1, seq2)
+      end
     end
 
     def assert_diffed(seq1, seq2)
-      assert_equal(seq2, diff_and_patch(seq1, seq2))
+      actual = diff_and_patch(seq1, seq2)
+      assert_equal(seq2, actual)
+      actual
     end
 
     def diff_and_patch(seq1, seq2)
@@ -67,7 +149,8 @@ if __FILE__ == $0
       puts "\e[3m #{__method__}(#{seq1.inspect}, #{seq2.inspect}) \e[0m"
 
       VDOM::TextDiff.diff(nil, seq1, seq2) do |patch|
-        p(patch)
+        d(patch:)
+        d(before: result)
 
         case patch
         in VDOM::Patches::InsertData[offset:, data:]
@@ -75,13 +158,14 @@ if __FILE__ == $0
         in VDOM::Patches::ReplaceData[offset:, count:, data:]
           result[offset...(offset + count)] = data
         in VDOM::Patches::DeleteData[offset:, count:]
-          result[offset..(offset + count)] = ""
+          result[offset...(offset + count)] = ""
         end
+        d(after: result)
       end
 
-      puts "\e[33mExpected: #{seq2.inspect}\e[0m"
-      puts "\e[33mActual:   #{result.inspect}\e[0m"
-      puts
+      d(expected: seq2)
+      d(actual: result)
+      d()
 
       result
     end
